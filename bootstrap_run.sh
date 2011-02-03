1205380099a716ea5d76b6f9f75a77049fcf1a8a
Baiju M <baiju.m.mail@gmail.com>
2011-02-03 13:36:40 +0000

    Installation instruction

--- bootstrap_run.sh
+++ bootstrap_run.sh
@@ -1,6 +1,10 @@
-echo "Creating Tables...."
-python create_tables.py
-echo "Populating Tables with data..."
-python populate_packages.py --demo
-echo "Running server..."
-python runserver.py
+echo "Creating virtualenv"
+virtualenv --clear --no-site-packages ve
+echo "Installing dependencies"
+./ve/bin/python setup.py develop
+echo "Creating tables"
+./ve/bin/python create_tables.py
+echo "Populating tables with data"
+./ve/bin/python populate_packages.py --demo
+echo "Running server"
+./ve/bin/python runserver.py
